3f06b338fb3e16597416c52a3031d25965abaf92
Hexagone001 <zvana5073@gmail.com>
2023-08-27 19:44:37 +0300

    Sakutin UI

---
+++ Assets/Resources/Scripts/GameData/Units/Princess/WeaponHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WeaponHandler : MonoBehaviour
+{
+    public UnityEvent OnDurabilityChanged = new();
+    public UnityEvent OnBroken = new();
+    [SerializeField] private WeaponScr weapon;
+    [SerializeField] private float attackSpeed;
+    [SerializeField] private int maxDurability;
+    [SerializeField] private int durability;
+    private void Awake()
+    {
+        if(weapon != null)
+        {
+            attackSpeed = weapon.AttackSpeed;
+            maxDurability = weapon.Durability;
+            durability = maxDurability;
+        }
+    }
+
+    public void StartAttack()
+    {
+        StartCoroutine(Attack());
+    }
+
+    private IEnumerator Attack()
+    {
+        yield return null;
+    }
+    public void TestAttack(int damage)
+    {
+        Durability -= damage;
+    }
+    public int Durability
+    {
+        get => durability;
+        set
+        {
+            durability = value;
+            if(durability <= 0)
+            {
+                OnBroken.Invoke();
+                durability = 0;
+            }
+            OnDurabilityChanged.Invoke();
+        }
+    }
+    public float MaxDurability => maxDurability;
+}
