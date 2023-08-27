ce33c1c4616169219d408716445a3395363db8b6
Hexagone001 <zvana5073@gmail.com>
2023-08-27 12:53:44 +0300

    Sakutin

---
+++ Assets/Resources/Scripts/GameData/Units/Princess/SakutinMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+namespace GameJam
+{
+    public class SakutinMovement : MonoBehaviour
+    {
+        [SerializeField] private UnitData sakutin;
+        [SerializeField] private float directionChangeDelay;
+        [SerializeField] private Vector3[] directions;
+        private Vector3 direction;
+
+        private void Awake()
+        {
+            StartCoroutine(ChangeDirection());
+        }
+        private IEnumerator ChangeDirection()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(directionChangeDelay);
+                direction = directions[Random.Range(0, directions.Length)];
+            }
+        }
+        private void Update()
+        {
+            MovementLogic();
+        }
+
+        private void MovementLogic()
+        {
+            if (direction.normalized != Vector3.zero)
+            {
+                transform.Translate(direction.normalized * sakutin.Speed * Time.deltaTime);
+            }
+        }
+    }
+}
+
